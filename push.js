bfb48d293d08fe41ed2393fa2c79292b9e093202
chinguyenvo <vochinguyen@flomail.net>
2022-08-25 21:38:34 +0700

    generate vapidKeys

---
+++ push.js
@@ -0,0 +1,9 @@
+var push = require("web-push");
+
+const vapidKeys = {
+  publicKey:
+    "BP6spkhPiUYrTtCt-DyNQv7347ETL8lk1iXq10YV3An21hNGJN_Kjiy8ty30rIR6l0A-jqg1oI0FLhJGO-tkpRM",
+  privateKey: "ujajCZL2YGkm18oo-63q2d7euIFhlUIJbXZXW0WLOGc",
+}; //push.generateVAPIDKeys();
+
+console.log(vapidKeys);
